c78700e9c3458ffa24221908aee83a0e93b8f199
k8xu <38595657+k8xu@users.noreply.github.com>
2019-07-25 19:23:13 -0400

    Preprocess data, extract features, and split dataset

--- cusboost_trial.py
+++ cusboost_trial.py
@@ -30,3 +30,41 @@ print("Checking account: ", credit_data['Checking account'].unique())
 # print("Duration: ", credit_data['Duration'].unique())
 print("Purpose: ", credit_data['Purpose'].unique())
 print("Risk: ", credit_data['Risk'].unique())
+
+# One hot encoding function
+def one_hot(df, nan = False):
+    original = list(df.columns)
+    category = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns = category, dummy_na = nan, drop_first = True)
+    new_columns = [c for c in df.columns if c not in original]
+    return df, new_columns
+
+# Feature extraction
+df = df.merge(pd.get_dummies(df['Sex'], drop_first=True, prefix='Sex'), left_index=True, right_index=True)
+df = df.merge(pd.get_dummies(df['Housing'], drop_first=True, prefix='Housing'), left_index=True, right_index=True)
+df = df.merge(pd.get_dummies(df["Saving accounts"], drop_first=False, prefix='Saving'), left_index=True, right_index=True)
+df = df.merge(pd.get_dummies(df["Checking account"], drop_first=False, prefix='Checking'), left_index=True, right_index=True)
+df = df.merge(pd.get_dummies(df['Purpose'], drop_first=True, prefix='Purpose'), left_index=True, right_index=True)
+
+del df['Sex']
+del df['Housing']
+del df['Saving accounts']
+del df['Checking account']
+del df['Purpose']
+
+# Map outputs to 0 (good) or 1 (bad)
+df = df.merge(pd.get_dummies(df.Risk, prefix='Risk'), left_index=True, right_index=True)
+del df['Risk']
+del df['Risk_good']
+
+# Separate X and y of dataset
+X = np.array(df.drop(['Risk_bad'], axis=1))
+y = np.array(df['Risk_bad'])
+print("X:", X, '\n')
+# print("y:", y, '\n')
+
+# Rescale feature values to decimals between 0 and 1
+normalization_object = Normalizer()
+X = normalization_object.fit_transform(X)
+# X = X_norm
+# print("X_norm:", X_norm)
